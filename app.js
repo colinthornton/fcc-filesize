5b018bbf018ffc3709626ba937354e96943e67e8
Colin Thornton <colinthorntontx@gmail.com>
2017-12-02 19:51:50 +0900

    Complete server functionality

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const app = express();
 const multer = require('multer');
+const upload = multer();
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.render('index');
+});
+
+app.post('/', upload.single('file'), (req, res) => {
+  res.json({ size: req.file.size });
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('Server open'));
